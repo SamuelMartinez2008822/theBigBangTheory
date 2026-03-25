599add88608e7a960165b59c1397340113b0e7b5
SamuelMartinez2008822 <sm6327279@gmail.com>
2026-03-25 13:18:41 -0500

    feat: Se agregaron estilos al boton de mostrar elenco y nueva funcionalidad para filtrar temporadas

--- src/app/components/list-episodes/list-episodes.component.ts
+++ src/app/components/list-episodes/list-episodes.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { Episodes } from '../../interfaces/episodes.interface';
 
 @Component({
@@ -6,6 +6,27 @@ import { Episodes } from '../../interfaces/episodes.interface';
   templateUrl: './list-episodes.component.html',
   styleUrl: './list-episodes.component.scss'
 })
-export class ListEpisodesComponent {
+export class ListEpisodesComponent implements OnChanges{
   @Input() episodeList: Episodes[];
+
+  selectedSeason: number | 'all' = 'all';
+  seasons: number[] = [];
+
+  ngOnChanges() {
+    if (this.episodeList?.length > 0) {
+      const uniqueSeasons = this.episodeList.map(ep => ep.season);
+      this.seasons = Array.from(new Set(uniqueSeasons)).sort((a, b) => a - b);
+    }
+  }
+
+  filterBySeason(season: number | 'all') {
+    this.selectedSeason = season;
+  }
+
+  get filteredEpisodes() {
+    if (this.selectedSeason === 'all') {
+      return this.episodeList;
+    }
+    return this.episodeList.filter(ep => ep.season === this.selectedSeason);
+  }
 }
